2bc00ea89edc86c6345abd46f4249b12e198cab4
Islam <islamibakaev@gmail.com>
2021-01-26 06:41:26 +0300

    added category page

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,36 @@
 import jQuery from 'jquery'
 import 'slick-carousel'
+import 'select2'
 import Slideout from 'slideout'
 
 (function ($, Slideout) {
 
 	function initSliders() {
-		var slider1NextArrowIcon = '<svg class="slider1__arrow-icon slider1__next-icon" width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.84627 12.0171L6.88898 7.00158L1.84628 1.98605" stroke="white" stroke-width="2" stroke-linecap="round" /></svg>'
-		var slider1PrevArrowIcon = '<svg class="slider1__arrow-icon slider1__prev-icon" width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.15373 1.98293L2.11102 6.99845L7.15373 12.014" stroke="white" stroke-width="2" stroke-linecap="round" /></svg>'
+		var sliderTheme1NextArrowIcon = '<svg class="slider-theme-1__arrow-icon slider-theme-1__next-icon" width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.84627 12.0171L6.88898 7.00158L1.84628 1.98605" stroke="white" stroke-width="2" stroke-linecap="round" /></svg>'
+		var sliderTheme1PrevArrowIcon = '<svg class="slider-theme-1__arrow-icon slider-theme-1__prev-icon" width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.15373 1.98293L2.11102 6.99845L7.15373 12.014" stroke="white" stroke-width="2" stroke-linecap="round" /></svg>'
 
-		$('.js-slider1').slick({
+		var sliderTheme2NextArrowIcon = '<svg class="slider-theme-2__arrow-icon slider-theme-2__next-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z" fill="#1D1919"/></svg>'
+		var sliderTheme2PrevArrowIcon = '<svg class="slider-theme-2__arrow-icon slider-theme-2__next-icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z" fill="#1D1919"/></svg>'
+
+		$('.js-slider-1').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
 			dots: true,
-			dotsClass: 'slider1__dots bottom-4',
-			nextArrow: '<button class="slider1__arrow slider1__next">' + slider1NextArrowIcon + '</button>',
-			prevArrow: '<button class="slider1__arrow slider1__prev">' + slider1PrevArrowIcon + '</button>',
+			dotsClass: 'slider-theme-1__dots bottom-4',
+			nextArrow: '<button class="slider-theme-1__arrow slider-theme-1__next">' + sliderTheme1NextArrowIcon + '</button>',
+			prevArrow: '<button class="slider-theme-1__arrow slider-theme-1__prev">' + sliderTheme1PrevArrowIcon + '</button>',
 		})
 
-		$('.js-slider2').slick({
+		$('.js-slider-2').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
 			infinite: true,
 			mobileFirst: true,
 			dots: true,
 			// arrows: false,
-			dotsClass: 'slider1__dots',
-			nextArrow: '<button class="slider1__arrow slider1__next">' + slider1NextArrowIcon + '</button>',
-			prevArrow: '<button class="slider1__arrow slider1__prev">' + slider1PrevArrowIcon + '</button>',
+			dotsClass: 'slider-theme-1__dots',
+			nextArrow: '<button class="slider-theme-1__arrow slider-theme-1__next">' + sliderTheme1NextArrowIcon + '</button>',
+			prevArrow: '<button class="slider-theme-1__arrow slider-theme-1__prev">' + sliderTheme1PrevArrowIcon + '</button>',
 			responsive: [{
 				breakpoint: 1200,
 				settings: {
@@ -61,19 +65,39 @@ import Slideout from 'slideout'
 			mobileFirst: true,
 			dots: true,
 			adaptiveHeight: true,
-			dotsClass: 'slider1__dots bottom-4',
-			nextArrow: '<button class="slider1__arrow slider1__next" style="right: 15px">' + slider1NextArrowIcon + '</button>',
-			prevArrow: '<button class="slider1__arrow slider1__prev" style="left: 15px">' + slider1PrevArrowIcon + '</button>',
+			dotsClass: 'slider-theme-1__dots bottom-4',
+			nextArrow: '<button class="slider-theme-1__arrow slider-theme-1__next" style="right: 15px">' + sliderTheme1NextArrowIcon + '</button>',
+			prevArrow: '<button class="slider-theme-1__arrow slider-theme-1__prev" style="left: 15px">' + sliderTheme1PrevArrowIcon + '</button>',
 			responsive: [{
 				breakpoint: 992,
 				settings: "unslick",
 			}]
 		})
+
+		$('.js-slider-4').slick({
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			mobileFirst: true,
+			nextArrow: '<button class="slider-theme-2__arrow slider-theme-2__next">' + sliderTheme2NextArrowIcon + '</button>',
+			prevArrow: '<button class="slider-theme-2__arrow slider-theme-2__prev">' + sliderTheme2PrevArrowIcon + '</button>',
+			responsive: [{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 3.9,
+				}
+			}, {
+				breakpoint: 320,
+				settings: {
+					slidesToShow: 2.5,
+				}
+			}]
+
+		})
 	}
 
-	$('.js-category-card-more').on('click', function(e) {
+	$('.js-category-block-more').on('click', function(e) {
 		var $target = $(e.target)
-		var $hidden = $target.closest('.js-category-card').find('.js-category-card-children').children('.d-none')
+		var $hidden = $target.closest('.js-category-block').find('.js-category-block-children').children('.d-none')
 		var toReveal = 6
 
 		e.preventDefault()
@@ -87,5 +111,9 @@ import Slideout from 'slideout'
 	})
 
 	initSliders()
+	$('.js-select').select2({
+		minimumResultsForSearch: -1,
+		width: "100%"
+	})
 	// console.log(Slideout)
 })(jQuery, Slideout)
